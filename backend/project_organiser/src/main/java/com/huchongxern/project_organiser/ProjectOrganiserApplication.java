7a7c7ec3dbc7249a393af114b9c8038496d6f645
ChongXern <chongxern@gmail.com>
2024-12-30 18:04:00 +0800

    Add Spring Initializr and IntelliJ Idea configurations

---
+++ backend/project_organiser/src/main/java/com/huchongxern/project_organiser/ProjectOrganiserApplication.java
@@ -0,0 +1,13 @@
+package com.huchongxern.project_organiser;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectOrganiserApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectOrganiserApplication.class, args);
+	}
+
+}
