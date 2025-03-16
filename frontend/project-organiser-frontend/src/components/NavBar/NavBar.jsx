b41a4718213ee439f48a35260eeca68184c5cabc
ChongXern <chongxern@gmail.com>
2025-03-16 16:03:51 +0800

    Finish navbar styling

--- frontend/project-organiser-frontend/src/components/NavBar/NavBar.jsx
+++ frontend/project-organiser-frontend/src/components/NavBar/NavBar.jsx
@@ -1,27 +1,12 @@
-import {React, useState, useEffect} from "react";
+import {React} from "react";
 import styles from './NavBar.module.css';
 
 import {getImageUrl, isImageValid} from "../../utils";
 
 export const NavBar = () => {
-    const [imageUrl, setImageUrl] = useState('');
-    const [isValid, setIsValid] = useState(false);
-
-    useEffect(() => {
-        const path = 'navBar/searchIcon.png';
-        const url = getImageUrl(path);
-
-        const checkImage = async () => {
-            const valid = await isImageValid(url);
-            setIsValid(valid);
-            setImageUrl(valid ? url : getImageUrl('default-image.png'));
-        };
-
-        checkImage();
-    }, []);
-    return <nav className={styles.NavBar}>
-        <div className={styles.titleContainer}>
-            <a className={styles.title} href="/"> ProjOrg </a>
+    return <nav className={styles.navBar}>
+        <div className={styles.title}>
+            <a href="/"> ProjOrg </a>
         </div>
         <div className={styles.rightItems}>
             <div className={styles.searchBar}>
